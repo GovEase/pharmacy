b01e42aee7881c4507a07d9af420f0343201aa63
ouharri <ouharrioutman@gmail.com>
2024-04-01 09:12:16 +0000

    Mapper interface for converting between UserRequest, UserResponsesDto, and User entities. Extends the generic _Mapper interface with UUID as the identifier type.

--- src/main/java/com/ouharri/pharma/mapper/UserMapper.java
+++ src/main/java/com/ouharri/pharma/mapper/UserMapper.java
@@ -1,8 +1,8 @@
-package com.ouharri.aftas.mapper;
+package com.ouharri.pharma.mapper;
 
-import com.ouharri.aftas.model.dto.requests.UserRequest;
-import com.ouharri.aftas.model.dto.responses.UserResponses;
-import com.ouharri.aftas.model.entities.User;
+import com.ouharri.pharma.model.dto.requests.UserRequest;
+import com.ouharri.pharma.model.dto.responses.UserResponsesDto;
+import com.ouharri.pharma.model.entities.User;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -11,7 +11,7 @@ import org.mapstruct.ReportingPolicy;
 import java.util.UUID;
 
 /**
- * Mapper interface for converting between {@link UserRequest}, {@link UserResponses}, and {@link User} entities.
+ * Mapper interface for converting between {@link UserRequest}, {@link UserResponsesDto}, and {@link User} entities.
  * Extends the generic {@link _Mapper} interface with UUID as the identifier type.
  */
 @Mapper(
@@ -19,5 +19,5 @@ import java.util.UUID;
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         componentModel = MappingConstants.ComponentModel.SPRING
 )
-public interface UserMapper extends _Mapper<UUID, UserRequest, UserResponses, User> {
+public interface UserMapper extends _Mapper<UUID, UserRequest, UserResponsesDto, User> {
 }
